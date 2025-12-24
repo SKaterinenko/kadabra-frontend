f6e9f28760371ecf65b3fe06163c889d56a415eb
SKaterinenko <skaterinenko@gmail.com>
2025-12-24 16:24:29 +0500

    Init. New version

--- src/components/Skeletons/HeadingSkeleton.jsx
+++
@@ -1,7 +0,0 @@
-import classes from './Skeletons.module.scss';
-
-export const HeadingSkeleton = ({className, ...rest}) => {
-    return (
-        <h1 className={classes.headingSkeleton} {...rest}></h1>
-    )
-}
--- src/components/Skeletons/ProductListSortSkeleton.jsx
+++
@@ -1,7 +0,0 @@
-import classes from './Skeletons.module.scss';
-
-export const ProductListSortSkeleton = ({ className }) => {
-    return (
-        <div className={`${classes.productListSortSkeleton}${className ? ' ' + className : ''}`}></div>
-    )
-}
